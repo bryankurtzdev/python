871c8ea586c171b76e679214b10d1d9414617343
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-07-14 14:37:38 -0300

    Update ex057.py

--- exercicios/ex057.py
+++ exercicios/ex057.py
@@ -1,8 +1,4 @@
-s = ''
-while s != 'M' and s != 'F':
-    s = str(input('Digite seu sexo [M/F]: ')).upper()
-    if s != 'M' and s != 'F':
-        print('O valor esta incorreto, por favor revise!')
-    elif s == 'M' or s == 'F':
-        print('Registrado!')
-print('Fim')
+s = str(input('Digite seu sexo [M/F]: ')).upper().strip()[0]
+while s not in 'MmFf':
+    s = str(input('Dados invalidos. Por favor, informe seu sexo [M/F]: ')).upper().strip()[0]
+print('Registrado!')
