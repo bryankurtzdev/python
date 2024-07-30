e321664f156b53340eecc6628a92d02e00c61a8a
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-07-30 14:01:42 -0300

    novos exercicios

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -1,2 +1,8 @@
 from random import randint
-num = (randint(0, 5), randint(0, 5), randint(0, 5), randint(0, 5), randint(0, 5))
+num = (randint(0, 10), randint(0, 10), randint(0, 10),
+       randint(0, 10), randint(0, 10))
+print('OS valores sorteados foram: ', end=' ')
+for c in num:
+    print(f'{c}', end=' ')
+print(f'\nO maior valor sorteado foi {max(num)}')
+print(f'O menor valor sorteado foi {min(num)}')
--- exercicios/ex075.py
+++ exercicios/ex075.py
@@ -1,5 +1,14 @@
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite um valor: '))
-n3 = int(input('Digite um valor: '))
-n4 = int(input('Digite um valor: '))
-num = (n1, n2, n3, n4)
+num = (int(input('Digite um valor: ')),
+    int(input('Digite um valor: ')),
+    int(input('Digite um valor: ')),
+    int(input('Digite um valor: ')))
+print(f'Voce digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')
+if 2 in num:
+    print(f'P valor 3 apareceu na posicao {num.index(3)}')
+else:
+    print(f'O valor 3 nao foi digitado em nenhuma posicao ')
+print('Os valores pares digitados foram ', end=' ')
+for n in num:
+    if n % 2 == 0:
+        print(n, end=' ')
