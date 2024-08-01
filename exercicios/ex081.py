5a1c8dca7c9bcd67a53a69a5217ea2f2d1ad27b7
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-08-01 14:36:14 -0300

    Update ex081.py

--- exercicios/ex081.py
+++ exercicios/ex081.py
@@ -4,10 +4,15 @@ while True:
     lista.append(int(input('Digite um valor: ')))
     r = str(input('Quer continuar? [S/N] ')).upper().strip()
     if r not in 'SN':
-        print('O valor digitado esta incorreto. Digite novamente.')
-        r = str(input('Quer continuar? [S/N] ')).upper().strip()
-        if r == 'N':
-            break
-print(f'N total foram digitados {lista.__len__()}')
-print(f'A lista e ordem decrescente fica {lista.sort(reverse=True)}')
-print(f'')
+        while r not in 'SNsn':
+            print('O valor digitado esta incorreto. Digite novamente.')
+            r = str(input('Quer continuar? [S/N] ')).upper().strip()
+    if r in 'Nn':
+        break
+print(f'No total foram digitados {len(lista)} numeros')
+lista.sort(reverse=True)
+print(f'A lista e ordem decrescente fica {lista}')
+if 5 in lista:
+    print(f'O valor cinco esta na lista')
+else:
+    print('O valor 5 nao esta na lista!')
